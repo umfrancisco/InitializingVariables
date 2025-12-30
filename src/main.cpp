25ada9befaa8b72a744a2b3146b3a7b42e2d0e4b
umfrancisco <francis.guitler@gmail.com>
2025-12-30 09:13:57 -0300

    calculate area of room

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
+	int room_width {0};
+	std::cout << "Enter the width of the room: ";
+	std::cin >> room_width;
+
+	int room_length {0};
+	std::cout << "Enter the length of the room: ";
+	std::cin >> room_length;
+
+	std::cout << "Total Area = " << room_width * room_length << std::endl;
 
 	return 0;
 }
